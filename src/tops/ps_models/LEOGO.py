d11b1efcbedea4f5873cb3b5e8e7c439c0f5761c
Sigrid Steffen <sigrid.steffen@sintef.no>
2025-06-18 15:55:35 +0200

    Further work with implementation of LEOGO

--- src/tops/ps_models/LEOGO.py
+++ src/tops/ps_models/LEOGO.py
@@ -14,13 +14,10 @@ def load():
             ['Drilling AC Bus B', 11],
             ['Terminal AC High Drilling B', 11],
             ['Terminal AC Low Drilling B', 0.69],
-            ['Terminal DC Drilling B', 1.3],
-            ['Drilling DC Bus B', 1.3],
             ['SG Terminal 3', 11],
             ['Terminal ACO_02', 11],
             ['Terminal AC High PEC_VSDc_GEX_02', 11],
             ['Terminal AC Low PEC_VSDc_GEX_02', 3.3],
-            ['Terminal DC PEC_VSDc_GEX_02', 6],
             ['Terminal TRA_UTL690_02', 11],
             ['Utility690 Bus B',  0.69],
             ['Terminal TRA_UTL400_02', 0.69],
@@ -30,77 +27,64 @@ def load():
 
         'lines': [
             ['name', 'from_bus', 'to_bus', 'length', 'S_n', 'V_n', 'unit', 'R', 'X', 'B'],
-            ['Cable WTG1', 'Terminal WindPark', 'Busbar WTG1 HV', 9, 27.95, 33, 'pf', 0.7074, 0.922869, 1.75e-3],
-            ['Line GEX_02', 'Main Bus B', 'Terminal AC High PEC_VSDc_GEX_02', 0.25, 9.907, 20, 'pf', 0.01965, 0.02356195, 1.75e-3],
-            ['Line ACO_02', 'Main Bus B', 'Terminal ACO_02', 0.25, 6.859, 11, 'pf', 0.038875, 0.02591815, 1.75e-3],
-            ['Line Utility B', 'Main Bus B', 'Terminal TRA_UTL690_02', 0.15, 6.859, 11, 'pf', 0.023325, 0.01555089, 1.75e-3],
-            ['Line 690 Trafo B', 'Utilty690 Bus B', 'Terminal TRA_UTL400_02', 0.15, 0.6215, 0.69, 'pf', 0.01179, 0.01413717, 1.75e-3],
-            ['Line SG3', 'SG Terminal 3', 'Main Bus B', 0.05, 39.63, 11, 'pf', 0.0009825, 0.0039275, 1.75e-3]
-            ['Line ACO_02', 'Main Bus B', 'Terminal ACO_02', 0.25, 6.859, 11, 'pf', 0.038875, 0.02591815, 1.75e-3], 
-            ['Line Drilling B', 'Main Bus B', 'Drilling AC Bus B', 0.25, 6.859, 11, 'pf', 0.038875, 0.02591815, 1.75e-3],
-            ['Line AC Drilling B', 'Drilling AC Bus B', 'Terminal AC High Drilling B', 0.05, 6.859, 11, 'pf', 0.007775, 0.00518363, 1.75e-3],
-            ['Line DC Drilling B', 'Terminal DC Drilling B', 'Drilling DC Bus B', 0.01, 6.755, 1.3, 'pf', 0.0001, 0.0002356, 1.75e-3]
+            ['Cable WTG1', 'Terminal WindPark', 'Busbar WTG1 HV', 9, 100, 33, 'pf', 0.0786, 0.102541, 0.3],
+            ['Line GEX_02', 'Main Bus B', 'Terminal AC High PEC_VSDc_GEX_02', 0.25, 100, 20, 'pf', 0.0786, 0.094247, 0.37],
+            ['Line ACO_02', 'Main Bus B', 'Terminal ACO_02', 0.25, 100, 11, 'pf', 0.1555, 0.103672, 0.28],
+            ['Line Utility B', 'Main Bus B', 'Terminal TRA_UTL690_02', 0.15, 100, 11, 'pf', 0.1555, 0.103672, 0.28],
+            ['Line 690 Trafo B', 'Utilty690 Bus B', 'Terminal TRA_UTL400_02', 100, 0.6215, 0.69, 'pf', 0.0786, 0.094247, 0.37],
+            ['Line SG3', 'SG Terminal 3', 'Main Bus B', 0.05, 100, 11, 'pf', 0.0786, 0.094247, 0.37],
+            ['Line ACO_02', 'Main Bus B', 'Terminal ACO_02', 0.25, 100, 11, 'pf', 0.1555, 0.103672, 0.28], 
+            ['Line Drilling B', 'Main Bus B', 'Drilling AC Bus B', 0.25, 100, 11, 'pf', 0.1555, 0.103672, 0.28],
+            ['Line AC Drilling B', 'Drilling AC Bus B', 'Terminal AC High Drilling B', 0.05, 100, 11, 'pf', 0.1555, 0.103672, 0.28]
 
         ],
 
         'transformers': [
             ['name', 'from_bus', 'to_bus', 'S_n', 'V_n_from', 'V_n_to', 'R', 'X'],
-            ['Trafo WTG1', 'Busbar WTG1 HV', 'Busbar WTG1 LV', 10, 0.69, 33, 0.5, 0.1],
-            ['Trafo WindPark', 'Terminal WindPark', 'Main Bus B', 25, 33, 11, 127, 0.1],
-            ['Trafo GEX_02', 'Terminal AC High PEC_VSDc_GEX_02', 'Terminal AC Low PEC_VSDc_GEX_02', 10.5, 11, 3.3, 0, 0],
-            ['Terminal AC Low PEC_VSDc_GEX_02', 'Terminal TRA_UTL690_02', 'Utility690 Bus B', 3.3, 11, 0.69, 4, 0.1],
-            ['TRA_UTL400_02', 'Utility690 Bus B', 'Utility400 SWBD/BusB', 0.6, 0.69, 0.4, 0.1, 0.1],
-            ['TRA_Dril_02', 'Terminal AC High Drilling B', 'Terminal AC Low Drilling B', 3.3, 11, 0.69, 0, 0]
+            ['Trafo WTG1', 'Busbar WTG1 HV', 'Busbar WTG1 LV', 10, 0.69, 33, 0.005, 0.0597913],
+            ['Trafo WindPark', 'Terminal WindPark', 'Main Bus B', 25, 33, 11, 0.002, 0.08997778],
+            ['Trafo GEX_02', 'Terminal AC High PEC_VSDc_GEX_02', 'Terminal AC Low PEC_VSDc_GEX_02', 10.5, 11, 3.3, 0.0047619, 0.05981074],
+            ['TRA_UTL690_02', 'Terminal TRA_UTL690_02', 'Utility690 Bus B', 3.3, 11, 0.69, 0.00606061, 0.1098329],
+            ['TRA_UTL400_02', 'Utility690 Bus B', 'Utility400 SWBD/BusB', 0.6, 0.69, 0.4, 0.00833333, 0.05941848],
+            ['TRA_Dril_02', 'Terminal AC High Drilling B', 'Terminal AC Low Drilling B', 3.3, 11, 0.69, 0.00606061, 0.1098329]
 
         ],
 
         'loads': [
             ['name', 'bus', 'P', 'Q', 'model'],
-            ['L1', 'B3', 1.05, 0, 'Z'],
-            ['L2', 'B3', 1.05, 0, 'Z'],
-            ['L3', 'B3', 0.25, 0, 'Z'],
-            ['L4', 'B3', 0.25, 0, 'Z']
+            ['DC Load - VSD_GEX_02', 'Terminal AC Low PEC_VSDc_GEX_02', 9.7, 0, 'Z'],
+            ['LOD2 - Utility Load 690V B', 'Utility690 Bus B', 1.049999, 0, 'Z'],
+            ['LOD4 - Utility Load 400V B', 'Utility400 SWBD/BusB', 0.25, 0, 'Z'],
+            ['ASM_ACO_02', 'Main Bus B', 0.5, 0.4, 'Z'],
+            ['ASM 690V B', 'Utility690 Bus B', 0.2, 0.1, 'Z']
+    
         ],
 
         'shunts': [
             ['name', 'bus', 'V_n', 'Q', 'model'],
-            ['C1', 'B3', 20, 5, 'Z'],
+            ['LCL-Kondensator Drilling B', 'Terminal AC Low Drilling B', 0.69, 0.139, 'Z'],
+            ['LCL-Kondensator GEX_02','Terminal AC Low PEC_VSDc_GEX_02', 3.3, 0.435, 'Z']
         ],
 
         'generators': {
             'GEN': [
                 ['name',    'bus',  'S_n',  'V_n',  'P',    'V',    'H',    'D',    'X_d',  'X_q',  'X_d_t',    'X_q_t',    'X_d_st',   'X_q_st',   'T_d0_t',   'T_q0_t',   'T_d0_st',  'T_q0_st'],
-                ['G1',      'B1',    28,    11,     15,    1.03,   6.5,    0,      1.8,    1.7,    0.3,        0.55,       0.25,       0.25,       8.0,        0.4,        0.03,       0.05],
-                ['G2',      'B1',    28,    11,     15,    1.03,   6.5,    0,      1.8,    1.7,    0.3,        0.55,       0.25,       0.25,       8.0,        0.4,        0.03,       0.05],
-                ['G3',      'B1',    28,    11,     15,    1.03,   6.5,    0,      1.8,    1.7,    0.3,        0.55,       0.25,       0.25,       8.0,        0.4,        0.03,       0.05],
-                ['G3',      'B1',    28,    11,     15,    1.03,   6.5,    0,      1.8,    1.7,    0.3,        0.55,       0.25,       0.25,       8.0,        0.4,        0.03,       0.05]
+                ['Synchronous Generator 3', 'Main Bus B', 28, 11, 20, 1.02, 6.5, 0, 2.33, 2.1, 0.173, 0, 0.159, 0.159, 0.822, 0, 0.03, 0.013]
             ]
         },
 
-        'vsc': {
-            'GridSideConverter_PV': [ 
-                ['name',   'bus',    'S_n',      "p_ref_grid",      "v_ref_grid",   'k_p',      'k_v',    'T_p',     'T_v',     'k_pll',   'T_pll',    'T_i',      "i_max"],
-                ['WT1',    'B12',      20,         0.0,               1.05,           5,          10,        0.1,        100,        5,        0.1,         0.01,      1.2],
-            ]
-        },
-
-        'gov': {'TGOV1': [
+        'gov': {'TGOV1': [ ## GAST -> TGOV1
             ['name',    'gen',  'R',    'D_t',  'V_min',    'V_max',    'T_1',  'T_2',  'T_3'],
-            ['GOV1',     'G1',   0.05,   0.02,   0,          1,          0.1,    0.09,   0.2]
+            ['GOV1',     'G1',   0.05,   0,   0, 1,    0.4,    3,   0]
             ]
         },
 
         'avr': {
-            'SEXS': [
+            'SEXS': [ ## ext_ST
                 ['name',   'gen',      'K',    'T_a',  'T_b',  'T_e',  'E_min',    'E_max'],
-                ['AVR1',    'G1',       100,    2.0,    10.0,   0.5,    -3,         3],
-            ]
-        },
-
-        'pss': {
-            'STAB1': [
-                ['name',    'gen',  'K',    'T',    'T_1',  'T_2',  'T_3',  'T_4',  'H_lim'],
-                ['PSS1',    'G1',   50,     10.0,   0.5,    0.5,    0.05,   0.05,   0.03]
+                ['Static Excitation System', 'Synchronous Generator 3', 50, 0.02, 0, 0.02, -1, 5],
             ]
         }
     }
+
+
